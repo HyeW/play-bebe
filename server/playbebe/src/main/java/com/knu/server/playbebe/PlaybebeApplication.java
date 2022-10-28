aa0317601a3350cfcd32746cd0833ee460c0aeb1
idoburnish <doyun7433@naver.com>
2022-10-28 17:00:53 +0900

    server init

---
+++ server/playbebe/src/main/java/com/knu/server/playbebe/PlaybebeApplication.java
@@ -0,0 +1,13 @@
+package com.knu.server.playbebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaybebeApplication.class, args);
+	}
+
+}
---
+++ server/playbebe/src/test/java/com/knu/server/playbebe/PlaybebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.server.playbebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaybebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
