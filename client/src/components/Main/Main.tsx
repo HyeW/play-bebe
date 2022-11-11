11be87d9feb23a17d34544038d6113dc8b91dca0
doheez <doheedev@gmail.com>
2022-11-11 21:07:20 +0900

    feat: Get latitude and longitude
    geolocation API를 사용하여 사용자의 현위치(위도, 경도) 정보를 얻는다.
    
    Task: #21

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,7 +1,14 @@
 import HotPlace from "../HotPlace/HotPlace";
 import {Container} from "@mui/material";
+import {useEffect} from "react";
+import {getLocation} from "../../utils/location";
 
-export default function() {
+export default function () {
+  useEffect(() => {
+    const gsLocation = getLocation();
+    console.info(`gsLocation: ${JSON.stringify(gsLocation)}`);
+  });
+
   return (
     <Container maxWidth="lg">
       <HotPlace/>
