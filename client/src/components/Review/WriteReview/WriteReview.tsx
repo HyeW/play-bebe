0ef9366278a670b199aee7afa4f0ee4c7c447657
doheez <doheedev@gmail.com>
2022-11-16 22:46:18 +0900

    feat: Create write review button
    텍스트 에디터 아래에 등록하기 버튼을 생성한다.
    
    Task: #35

--- client/src/components/Review/WriteReview/WriteReview.tsx
+++ client/src/components/Review/WriteReview/WriteReview.tsx
@@ -1,7 +1,9 @@
-import {Box, Container} from "@mui/material";
+import {Box, Button, Container} from "@mui/material";
 import ReviewInput from "./ReviewInput";
 import ReviewTextEditor from "./ReviewTextEditor";
 import "./WriteReview.css";
+import CreateIcon from "@mui/icons-material/Create";
+import React from "react";
 
 export default function WriteReview() {
   return (
@@ -9,7 +11,21 @@ export default function WriteReview() {
       <Box my={7} p={6} sx={{border: 1, borderRadius: 1, borderColor: 'secondary.dark'}}>
         <ReviewInput/>
         <ReviewTextEditor/>
+        <WriteButton handleClickWriteButton={()=>{}}/>
       </Box>
     </Container>
   );
+}
+
+function WriteButton(props: {
+  handleClickWriteButton: ()=>void,
+}) {
+  return (
+    <Box sx={{display: 'flex', justifyContent: 'center'}} mt={5}>
+      <Button variant="contained" size="large" disableElevation
+              sx={{borderRadius: 10, px: 10, py: 1.5}}
+              startIcon={<CreateIcon/>}
+              onClick={props.handleClickWriteButton}>{'등록하기'}</Button>
+    </Box>
+  )
 }
