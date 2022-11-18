21fdc070336114a83876f562350c9fc64a0de45b
doheez <doheedev@gmail.com>
2022-11-19 01:00:45 +0900

    feat: Set up proxy with server url
    Task: #54

---
+++ client/src/setupPropxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const BASE_URL = '3.34.134.109'
+// const LOCAL_URL = 'localhost';
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware('/api', {
+            target: `http://${BASE_URL}:8080`,
+            changeOrigin: true
+        })
+    );
+};
