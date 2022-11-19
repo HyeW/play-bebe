c50aa87862ec6cfdbc95de6000b2a93607a42262
idoburnish <doyun7433@naver.com>
2022-11-19 22:52:23 +0900

    refactor: add place id in PlaceSimpleInfoDto
    상세보기 페이지로 전환할 때 필요한 place id 추가

--- server/playbebe/src/main/java/com/knu/server/playbebe/recommend/dto/PlaceSimpleInfoDto.java
+++ server/playbebe/src/main/java/com/knu/server/playbebe/recommend/dto/PlaceSimpleInfoDto.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @NoArgsConstructor
 public class PlaceSimpleInfoDto implements Comparable<PlaceSimpleInfoDto> {
 
+    private Long id;
     private String name;
     private String simpleAddress;
     private Double totalRating;
@@ -22,6 +23,7 @@ public class PlaceSimpleInfoDto implements Comparable<PlaceSimpleInfoDto> {
     private String distanceString;
 
     public PlaceSimpleInfoDto(Place place, double latitude, double longitude) {
+        this.id = place.getId();
         this.name = place.getEstablishmentName();
         this.simpleAddress = getSimpleAddress(place.getAddress());
         this.totalRating = Math.round(place.getTotalRating() * 10) / 10.0;
