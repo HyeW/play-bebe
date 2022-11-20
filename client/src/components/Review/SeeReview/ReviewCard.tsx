c91b73a5fa3525988de2334431da7f57d2638def
doheez <doheedev@gmail.com>
2022-11-21 00:13:46 +0900

    feat: Connect review api with view
    Task: #54

--- client/src/components/Review/SeeReview/ReviewCard.tsx
+++ client/src/components/Review/SeeReview/ReviewCard.tsx
@@ -1,26 +1,38 @@
-import {Box, Grid, Rating, Skeleton, Stack, Typography} from "@mui/material";
+import {Box, Grid, Rating, Stack, Typography} from "@mui/material";
 import React from "react";
 
-export interface ReviewCardProps extends ReviewTitleProps, ReviewContentProps, ReviewInfoProps {
+export interface ReviewCardProps extends ReviewTitleProps, ReviewContentProps, ReviewInfoProps, ReviewImageProps {
   isOnePlace?: boolean,
 }
 
-export default function ReviewCard({placeName, address, content, visitDate, createDate, nickname, rating, index, isOnePlace}: ReviewCardProps) {
+export default function ReviewCard({
+                                     establishmentName,
+                                     address,
+                                     content,
+                                     visitDate,
+                                     createDate,
+                                     nickname,
+                                     rating,
+                                     index,
+                                     isOnePlace,
+                                     reviewId
+                                   }: ReviewCardProps) {
   return (
     <Box my={5}>
-      {!isOnePlace && <ReviewTitle placeName={placeName} address={address}/>}
+      {!isOnePlace && <ReviewTitle establishmentName={establishmentName} address={address}/>}
       <Grid container spacing={3}>
-        {index !== undefined && index%2 === 0 &&
+        {index !== undefined && index % 2 === 0 &&
             <Grid item xs>
-                <ReviewImage/>
+                <ReviewImage reviewId={reviewId}/>
             </Grid>}
         <Grid item xs>
           <ReviewContent content={content}/>
-          <ReviewInfo visitDate={visitDate} createDate={createDate} nickname={nickname} rating={rating} index={index}></ReviewInfo>
+          <ReviewInfo visitDate={visitDate} createDate={createDate} nickname={nickname} rating={rating}
+                      index={index}></ReviewInfo>
         </Grid>
-        {index !== undefined && index%2 === 1 &&
+        {index !== undefined && index % 2 === 1 &&
             <Grid item xs>
-                <ReviewImage/>
+                <ReviewImage reviewId={reviewId}/>
             </Grid>}
       </Grid>
     </Box>
@@ -28,18 +40,18 @@ export default function ReviewCard({placeName, address, content, visitDate, crea
 }
 
 interface ReviewTitleProps {
-  placeName: string,
+  establishmentName: string,
   address: string,
 }
 
-export const ReviewTitle = ({placeName, address}: ReviewTitleProps) => {
+export const ReviewTitle = ({establishmentName, address}: ReviewTitleProps) => {
   return (
     <Box mb={1}>
       <Typography
         variant="h6"
         color="primary"
         sx={{textDecoration: 'underline'}}>
-        &nbsp;&nbsp;{placeName}&nbsp;&nbsp;
+        &nbsp;&nbsp;{establishmentName}&nbsp;&nbsp;
       </Typography>
       <Typography>
         &nbsp;&nbsp;{address}
@@ -48,9 +60,15 @@ export const ReviewTitle = ({placeName, address}: ReviewTitleProps) => {
   );
 };
 
-const ReviewImage = () => {
+interface ReviewImageProps {
+  reviewId: number,
+}
+
+const ReviewImage = ({reviewId}: ReviewImageProps) => {
   return (
-    <Skeleton variant="rectangular" width="100%" height={500} sx={{mt: 1}}/>
+    <Box mt={1}>
+      <img src={`/api/review/image/${reviewId}`} width="100%" height={500} alt=""></img>
+    </Box>
   )
 }
 
@@ -74,7 +92,7 @@ interface ReviewInfoProps {
 
 const ReviewInfo = ({visitDate, createDate, nickname, rating, index}: ReviewInfoProps) => {
   return (
-    <Stack sx={{display: 'flex', alignItems: index && (index%2 === 1) ? 'start' : 'end', mt: 3}}>
+    <Stack sx={{display: 'flex', alignItems: index && (index % 2 === 1) ? 'start' : 'end', mt: 3}}>
       <Typography color="text.secondary">방문일자 {visitDate}</Typography>
       <Typography color="text.secondary">작성일자 {createDate}</Typography>
       <Typography color="text.secondary">작성자 {nickname}</Typography>
