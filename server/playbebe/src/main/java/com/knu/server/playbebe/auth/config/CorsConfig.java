308c5c7665d10796d69299335d957f87443a6120
HyeW <qorgp13@naver.com>
2022-11-20 19:21:17 +0900

    feat: Add getting user id API 유저 이메일을 통해 유저ID를 가져오는 API를 구현한다.
    Task: #62

--- server/playbebe/src/main/java/com/knu/server/playbebe/auth/config/CorsConfig.java
+++ server/playbebe/src/main/java/com/knu/server/playbebe/auth/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*"); //모든 ip에 응답을 허용
+        config.addAllowedOriginPattern("*"); //모든 ip에 응답을 허용
         config.addAllowedHeader("*"); //모든 헤더를 허용
         config.addAllowedMethod("*"); //restful api method 다 허용
         source.registerCorsConfiguration("/*",config);
